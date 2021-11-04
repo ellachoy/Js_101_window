2fa1e66f1b735301ced647af87077cf766157eb5
Ella Choy <egal21de@gmail.com>
2021-11-04 16:48:12 +0100

    fun2

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ printHello();
 function changeName(obj){
     obj.name='coder'
 }
+const ellie ={ name:'ellie'};
+changeName(ellie);
+console.log(ellie)
+
+//default parameter
+function showMessage(message, from='unknown'){
+    console.log(`${message} by ${from}`);
+}
+showMessage('Hi');
+
+// rest parameters
+function printAll(...args) {
+    // for (let i=0; i < args.length; i++){
+    //     console.log(args[i]);
+    // }
+    for (const arg of args) {
+        console.log(arg);
+    }
+}
+printAll('javascript', 'function','ella')
